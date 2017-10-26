902ad1aa0ed4520dd8b1f87e728ea880926dc745
Mike Gozzo <mike@smooch.io>
2017-10-25 19:31:53 -0600

    subdomain-wide auth

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(methodOverride('_method'));
-app.use(cookieSession({keys: [process.env.SESSION_SECRET], resave: true, saveUninitialized: true, cookie: {signed: false, domain:'volunteercheck.org'}}));
+//app.use(cookieSession({keys: [process.env.SESSION_SECRET], resave: true, saveUninitialized: true, cookie: {signed: false, domain:'volunteercheck.org'}}));
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', req.headers.origin);
@@ -75,7 +75,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-//app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true, cookie: {domain:'.example.com'} }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true, cookie: {domain:'.volunteercheck.org'} }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
