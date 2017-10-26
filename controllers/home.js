545121a61d9af74464c0de56e30c98cbebe6e7d0
Mike Gozzo <mike@smooch.io>
2017-10-25 19:12:43 -0600

    subdomain-wide auth

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,9 @@
  * GET /
  */
 exports.index = function(req, res) {
+  console.log("SHOW HOME");
+  console.log(req.cookies);
+
   if(req.isAuthenticated()) {
     if(req.user.org) {
       res.redirect('/orgs/'+req.user.org+"/applications");
